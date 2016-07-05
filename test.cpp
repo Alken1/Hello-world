5e2d626410c6c439c03879b2dcc1dcdcffcf698c
Alken1 <mikami85@mail.ru>
2016-07-05 13:55:02 +0400

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : User
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i=0; i<5;i++)
+	cout << "i=" <<i<< endl; // prints !!!Hello World!!!
+	return 0;
+}
