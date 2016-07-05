2b77a0c6d993a884b58fa56590b41b82acbde951
Alken1 <mikami85@mail.ru>
2016-07-05 12:57:04 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	for (int i=0; i<5; i++)
+	cout << "i=" <<i<< endl; // prints !!!Hello World!!!
 	return 0;
 }
